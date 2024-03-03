b34c43b2f46cabf7d3e6c9dc90e2da6c9eb2f321
YUVASRI06 <yuvasriperumalsamy0609@gmail.com>
2024-03-03 20:33:44 +0530

    drryery

--- basic.java
+++ basic.java
@@ -4,6 +4,14 @@ public class EvenOdd {
             System.out.println(i);
              System.out.println("Hello, " + name + "! Welcome.");
 
+   if (num >= 18) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are a minor.");
+			 System.out.println("You are a minor.");
+			
+			
+        }
              
       }
     }
